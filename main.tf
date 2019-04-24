d547419a4df88a935deba9da2daa35e556b3f384
jianghaitao1221 <jianghaitao@kinsoft.com>
2019-04-24 11:32:44 +0800

    Change archive_file output_path to relative path

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ data "archive_file" "push_lambda" {
     filename = "${var.lambda_python_file}"
   }
 
-  output_path = "${path.module}/function/push.zip"
+  output_path = "${substr("${path.module}/function/push.zip", length(path.cwd) + 1, -1)}"
 }
 
 data "aws_iam_policy_document" "push_lambda" {
